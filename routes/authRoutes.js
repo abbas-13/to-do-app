35a3cc8cf0440b8e6be7fce5307555e32091ed14
m-abbas-13 <abbas.m@goodbusinesslab.org>
2026-02-02 00:29:38 +0530

    something

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,7 +13,7 @@ export default (app) => {
     "/auth/google/callback",
     passport.authenticate("google"),
     (req, res) => {
-      res.redirect("/");
+      res.redirect(`${process.env.CLIENT_SIDE_URL}/`);
     },
   );
 
