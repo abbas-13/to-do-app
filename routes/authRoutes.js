bd2c759be7641f8c494b38f6df3bb323537f6c0c
m-abbas-13 <abbas.m@goodbusinesslab.org>
2026-02-01 23:40:09 +0530

    add redirect back

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -22,11 +22,7 @@ export default (app) => {
       console.log("req.user exists:", !!req.user);
       console.log("req.session content:", req.session);
 
-      res.json({
-        success: true,
-        redirect: process.env.CLIENT_SIDE_URL,
-        user: req.user.id,
-      });
+      res.redirect(process.env.CLIENT_SIDE_URL);
     },
   );
 
