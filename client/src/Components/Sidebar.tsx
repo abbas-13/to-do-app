6208ef346666f24e8fb581b017d4149f28e85bdf
m-abbas-13 <abbas.m@goodbusinesslab.org>
2026-02-07 20:41:59 +0530

    add theme change option on mobile

--- client/src/Components/Sidebar.tsx
+++ client/src/Components/Sidebar.tsx
@@ -1,8 +1,9 @@
 import { useContext, useState } from "react";
-import { Menu, Plus } from "lucide-react";
+import { Menu, Moon, Plus, Sun } from "lucide-react";
 import { useNavigate } from "react-router";
 import { toast } from "sonner";
 
+import { useTheme } from "@/Components/ui/theme-provider";
 import { ToDoList } from "./To-DoList";
 import { SearchBar } from "./SearchBar";
 import { Button } from "@/Components/ui/button";
@@ -11,6 +12,7 @@ import { ListsContext } from "@/Context/ListsContext";
 import { SelectListContext } from "@/Context/SelectListContext";
 import { useIsMobile } from "@/hooks/use-mobile";
 import type { ListsStateType } from "@/assets/Types";
+import { Switch } from "./ui/switch";
 
 export const CustomSidebar = () => {
   const [input, setInput] = useState("");
@@ -19,6 +21,7 @@ export const CustomSidebar = () => {
   const { lists, setLists } = useContext(ListsContext);
   const { selectList, setSelectedList } = useContext(SelectListContext);
   const { toggleSidebar } = useSidebar();
+  const { theme, setTheme } = useTheme();
   const isMobile = useIsMobile();
   const navigate = useNavigate();
 
@@ -113,43 +116,86 @@ export const CustomSidebar = () => {
     }
   };
 
+  const toggleTheme = (isChecked: boolean) => {
+    const selectedTheme = isChecked ? "light" : "dark";
+    setTheme(selectedTheme);
+  };
+
   const sideBarContent = () => {
     return (
-      <>
-        <SearchBar
-          setSearchResult={setSearchResults}
-          lists={lists}
-          input={input}
-          setInput={setInput}
-        />
-        <div className="flex my-4 items-center justify-center w-full">
-          <Button
-            className="bg-foreground hover:bg-[#FFFFFF] hover:border-2 hover:border-[#2097f3] active:bg-[#2097f3] active:text-white hover:text-black active:outline-2 active:outline-[#85C7F8] hover:shadow-lg active:shadow-none active:border-1 active:border-white text-white"
-            variant="outline"
-            onClick={addList}
-          >
-            Create List
-            <Plus strokeWidth={3} />
-          </Button>
+      <div className="h-full flex justify-between flex-col">
+        <div>
+          {isMobile && (
+            <>
+              <div className="flex gap-4 px-4 my-4 justify-center w-full items-center">
+                <img style={{ height: "30px" }} src="/check.png" />
+                <h1 className="bg-gradient-to-r from-[#2097F3] to-[#60B4F5] bg-clip-text text-transparent text-transparent text-3xl font-semibold">
+                  to-do
+                </h1>
+              </div>
+              <div className="border border-gray-200 m-2 mb-4"></div>
+            </>
+          )}
+
+          <SearchBar
+            setSearchResult={setSearchResults}
+            lists={lists}
+            input={input}
+            setInput={setInput}
+          />
+          <div className="flex my-4 items-center justify-center w-full">
+            <Button
+              className="bg-foreground hover:bg-[#FFFFFF] hover:border-2 hover:border-[#2097f3] active:bg-[#2097f3] active:text-white hover:text-black active:outline-2 active:outline-[#85C7F8] hover:shadow-lg active:shadow-none active:border-1 active:border-white text-white"
+              variant="outline"
+              onClick={addList}
+            >
+              Create List
+              <Plus strokeWidth={3} />
+            </Button>
+          </div>
+          {input?.length
+            ? searchResults?.map((item) => (
+                <ToDoList
+                  key={item._id}
+                  list={item}
+                  createList={createList}
+                  deleteList={deleteList}
+                />
+              ))
+            : lists?.map((list: ListsStateType) => (
+                <ToDoList
+                  key={list._id}
+                  list={list}
+                  createList={createList}
+                  deleteList={deleteList}
+                />
+              ))}
         </div>
-        {input?.length
-          ? searchResults?.map((item) => (
-              <ToDoList
-                key={item._id}
-                list={item}
-                createList={createList}
-                deleteList={deleteList}
-              />
-            ))
-          : lists?.map((list: ListsStateType) => (
-              <ToDoList
-                key={list._id}
-                list={list}
-                createList={createList}
-                deleteList={deleteList}
-              />
-            ))}
-      </>
+        {isMobile && (
+          <div>
+            <div className="border border-gray-200 m-2"></div>
+            <div className="flex justify-between w-full p-2 px-4 mb-2">
+              <div>Theme</div>
+              <div
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+                className="flex justify-around text-xs gap-2 items-center"
+              >
+                <Moon size={18} />
+                <Switch
+                  checked={theme === "light" ? true : false}
+                  onCheckedChange={(checked) => {
+                    event?.stopPropagation();
+                    toggleTheme(checked);
+                  }}
+                />
+                <Sun size={18} />
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     );
   };
 
@@ -157,9 +203,9 @@ export const CustomSidebar = () => {
     <>
       {isMobile ? (
         <div className="h-screen flex">
-          <div className="pt-6 pl-4 bg-[#F5FAFE]">
+          <div className="pt-6 pl-4 bg-background">
             <Sidebar>
-              <SidebarContent className="gap-0 w-[230px]!">
+              <SidebarContent className="gap-0 w-[230px]! bg-secondary">
                 {sideBarContent()}
               </SidebarContent>
             </Sidebar>
