035efaabb1e4abb321f62993a409628fa05aa056
m-abbas-13 <abbas.m@goodbusinesslab.org>
2026-02-07 20:20:13 +0530

    server side changes

--- models/user.js
+++ models/user.js
@@ -1,11 +1,22 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  googleId: { type: String, required: true },
-  displayName: { type: String, required: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true },
+  googleId: String,
+  githubId: String,
+  username: String,
+  local: {
+    password: String,
+    email: String,
+    name: String,
+  },
+  displayName: String,
+  name: String,
+  email: String,
 });
+
+userSchema.index({ googleId: 1 }, { unique: true, sparse: true });
+userSchema.index({ githubId: 1 }, { unique: true, sparse: true });
+userSchema.index({ "local.email": 1 }, { unique: true, sparse: true });
 
 const User = mongoose.model("User", userSchema);
 export default User;
